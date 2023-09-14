509d372adc3d04a3db8c657e4f69e6a3bcd72bc8
ProgrammingHero1 <mamun2k9kuet@gmail.com>
2023-09-15 05:18:46 +0600

    added styled cart section by using tailwind css

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -8,15 +8,22 @@ const Cart = ({
 }) => {
   console.log(selectedCourses);
   return (
-    <div className="w-full bg-base-100 shadow-xl rounded-xl">
-      <h1>Credit hours remaining:{remainingCredit} </h1>
-      <hr />
-      <h1>Courses Name:</h1>
+    <div className="w-full bg-base-100 shadow-xl rounded-xl p-6">
+      <h1 className="text-lg font-bold text-[#2F80ED]">
+        Credit Hours Remaining: {remainingCredit}
+        {remainingCredit > 1 ? " hrs" : " hr"}
+      </h1>
+      <div className="my-4 border-b-2"></div>
+      <h1 className="text-xl font-bold mb-5">Courses Name:</h1>
       {selectedCourses.map((course, idx) => (
-        <p key={idx}>{course.title}</p>
+        <li className="list-decimal leading-loose" key={idx}>
+          {course.title}
+        </li>
       ))}
-      <h1>Credit:{totalCredit}</h1>
-      <h1>Total Price{totalPrice}</h1>
+      <div className="mt-6 mb-4 border-b-2"></div>
+      <h1>Total Credit Hour : {totalCredit}</h1>
+      <div className="my-4 border-b-2"></div>
+      <h1>Total Price: {totalPrice} USD</h1>
     </div>
   );
 };
